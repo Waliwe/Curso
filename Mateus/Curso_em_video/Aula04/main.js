d80b1ba7cda96fec2dedd1c32e762c6c29eb82be
Mateus.Agnol <mateusdall12@gmail.com>
2022-01-27 15:50:46 -0300

    Add new

--- Mateus/Curso_em_video/Aula04/main.js
+++ Mateus/Curso_em_video/Aula04/main.js
@@ -1,3 +1,3 @@
 window.alert('Minha primeira messagem')
-window.confirm('Esta gostando de JS?')
-window.prompt('Qual é o seu nome')
+window.confirm('Esta gostando de JS?') //ok e cancel
+window.prompt('Qual é o seu nome')  // pergunta o nome
