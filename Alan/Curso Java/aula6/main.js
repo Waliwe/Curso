c645c833871243de94d057d635890896a39597f9
Alan Gabriel Makus <alangabrielmakus901@gmail.com>
2022-01-27 16:55:06 -0300

    mudança e adição do Main.js

---
+++ Alan/Curso Java/aula6/main.js
@@ -0,0 +1,4 @@
+var n1 = Number.parseInt(Windows.prompt('digite um número : '));
+var n2 = Number.parseInt(Windows.prompt('digite mais um número : '));
+var soma = n1 + n2;
+window.alert('a soma dos números é de : ' + soma)
