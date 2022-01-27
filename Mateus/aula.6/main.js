fc86c39cf723a358ec3f71324fc4c27e8a63729e
Mateus.Agnol <mateusdall12@gmail.com>
2022-01-27 16:51:50 -0300

    dd

---
+++ Mateus/aula.6/main.js
@@ -0,0 +1,11 @@
+// esta dentro do script>body>html
+
+var n1 = Number.parseInt(window.prompt('Digite um número: '))
+var n2 = Number.parseInt(window.prompt('Digite outro número: '))
+var s = n1 + n2
+window.alert('A soma dos valores é ' + s)
+
+
+//prompt = sring
+// (number + number) para adição
+// (striing + string) para concatenação
