0bf14eceb43ac3582d70809961168b6049466de5
Alan Gabriel Makus <alangabrielmakus901@gmail.com>
2022-01-27 16:59:07 -0300

    remoção do curso

--- Alan/Curso Java/aula6/main.js
+++
@@ -1,4 +0,0 @@
-var n1 = Number.parseInt(Windows.prompt('digite um número : '));
-var n2 = Number.parseInt(Windows.prompt('digite mais um número : '));
-var soma = n1 + n2;
-window.alert('a soma dos números é de : ' + soma)
